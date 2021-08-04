2059279781dc1ed2212c5c1f11b677b9aa0a5925
Na <na@MacBook-Air-cua-Na.local>
2021-08-04 23:53:32 +0900

    Add displayInformation.py file

---
+++ displayInformation.py
@@ -0,0 +1,4 @@
+print ('Họ tên    : Trần Thu Hằng')
+print ('Ngày sinh : 25-08')
+print ('Địa chỉ.  : Nhật Bản')
+print ('Mục đích đến với khoá học Python : phát triển thiết bị IoT bằng Python và tìm hiểu về máy học')
