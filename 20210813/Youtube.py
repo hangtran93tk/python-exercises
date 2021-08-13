89964ae40667ae5ee5b42e8b306f5c40eeb01cff
18jz0127 ハン <hangtrand6qt1@gmail.com>
2021-08-13 21:52:43 +0900

    upload new folder

--- 20210813/Youtube.py
+++ 20210813/Youtube.py
@@ -8,11 +8,11 @@ try:
         webbrowser.open("https://m.youtube.com/watch?v=G5MyiQQCmPA&list=PLIEkkiNDdc--WAuWctsnPJL00PnBnBOIc&index=1")
         time.sleep(10)
         # 　os.system("taskkill /im chrome.exe /f")
-        os.system("killall chrome.exe")
+        os.system("pkill -f 'PATTERN'")
         webbrowser.open("https://m.youtube.com/watch?v=G5MyiQQCmPA&list=PLIEkkiNDdc--WAuWctsnPJL00PnBnBOIc&index=2")
         time.sleep(10)
         # 　os.system("taskkill /im chrome.exe /f")
-        os.system("killall chrome.exe")
+        os.system("pkill -f 'PATTERN'")
 except KeyboardInterrupt as e:
     sys.exit()
 
