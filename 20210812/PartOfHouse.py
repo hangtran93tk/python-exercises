e46b207ddb183ff49e1cc0d6616cb39351efb9f1
18jz0127 ハン <hangtrand6qt1@gmail.com>
2021-08-17 00:35:09 +0900

    upload file DreamHouse4.py

---
+++ 20210812/PartOfHouse.py
@@ -0,0 +1,41 @@
+def part1(t) :
+    t.right(90)
+    t.forward(250)
+    t.left(90)
+    t.forward(400)
+    t.left(90)
+    t.forward(250)
+    t.left(90)
+    t.forward(400)
+    t.right(90)
+
+def part2(t):
+    t.right(45)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.left(180)
+    t.forward(200)
+    t.right(135)
+    t.forward(259)
+    t.right(90)
+    t.forward(142)
+
+def part3(t):
+    for i in range(2):
+        t.right(90)
+        t.forward(200)
+        t.right(90)
+        t.forward(150)
+
+def part4(t):
+    t.right(90)
+    t.forward(75)
+    t.left(90)
+    t.forward(15)
+    t.left(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(15)
+    t.right(90)
+    t.forward(75)
