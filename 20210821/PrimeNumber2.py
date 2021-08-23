9e187d09474ea598958f9df46c9b4718b2a4ddaa
18jz0127 ハン <hangtrand6qt1@gmail.com>
2021-08-23 21:44:54 +0900

    Upload new file

--- 20210821/PrimeNumber2.py
+++ 20210821/PrimeNumber2.py
@@ -1,14 +1,13 @@
 
 num = int(input("Hay nhap mot so tu nhien lon hon 0: "))
 
-for i in range(2,num + 1,1):
-    if i == 2:
-        print(i, end=", ")
+count = 0
+n = 1
+while count < num:
+    n += 1
+    for i in  range(2, n // 2 + 1):
+        if n % i == 0:
+            break
     else:
-        a = i // 2 + 1
-        for j in (2,a, 1):
-            if i % j == 0:
-                break
-            else:
-                print(i, end=", ")
-                break
+        print(n, end=" ")
+        count += 1
