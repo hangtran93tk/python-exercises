65062a4e1f2b2108007b318ae037fc09c2da3e0a
18jz0127 ハン <hangtrand6qt1@gmail.com>
2021-09-14 23:03:49 +0900

    Upload new file

--- 20210911/Ex5.py
+++ 20210911/Ex5.py
@@ -1,10 +1,14 @@
 tuple1 = ((1,2,3,4),(3,5,2,1),(2,2,3,1))
 
 def sum_row_elements(tuple):
-    sum_list = []
-    sum_row = 0
-    j = 0
-    for i in range(len(tuple)):
-        print(tuple[i][i:])
+    new_list = []
+    column_sum = 0
+    for i in range(len(tuple[0])):
+        for j in range(len(tuple)):
+            column_sum += tuple[j][i]
+        new_list.append(column_sum)
+        column_sum = 0
+    return new_list
 
-sum_row_elements(tuple1)
+new_tuple = tuple(sum_row_elements(tuple1))
+print(new_tuple)
